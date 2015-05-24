b708ad4c5b2f5b2acb4e8512d80888efe3b0dc4c
li3ro <liero.cphv@gmail.com>
2015-05-24 09:28:46 +0300

    handled licensing issues

--- AndorsTrail_rewarded/src/com/gpl/rpg/AndorsTrail/model/ability/ActorConditionType.java
+++ AndorsTrail_rewarded/src/com/gpl/rpg/AndorsTrail/model/ability/ActorConditionType.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright© 2015 Yaniv Bokobza
+ * Based on Andor's Trail open source game (GPLv2)
+ *
+ * This file is part of Andor's Trail - Rewarded.
+ *
+ * Andor's Trail - Rewarded is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Andor's Trail - Rewarded is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Andor's Trail - Rewarded.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.gpl.rpg.AndorsTrail.model.ability;
 
 import com.gpl.rpg.AndorsTrail.model.ability.traits.AbilityModifierTraits;
--- AndorsTrail_rewarded/src/com/gpl/rpg/AndorsTrail/util/LruCache.java
+++ AndorsTrail_rewarded/src/com/gpl/rpg/AndorsTrail/util/LruCache.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.gpl.rpg.AndorsTrail.util;
 
 import java.util.LinkedHashMap;
